8e26e146f4ea544133ccb1cb9603cf4b3f834b3e
jdgnpy <616dona@gmail.com>
2021-11-16 14:48:51 -0300

    Add - form para busqueda de pelicula

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  const [pelicula, setPelicula] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    //console.log(pelicula);
+    setResult('Mostrando resultados para ' + pelicula);
+  }
+
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <h1>Info Peliculas</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input 
+          id="input"
+          onChange={(e) => setPelicula(e.target.value)}
+          value={pelicula}
+          placeholder="Ex: 'Pulp Fiction'"
+        />
+        <input type="submit" value="Buscar"/>
+      </form>
+      <p>{result}</p>
+
+
+    </>
   );
 }
+
 
 export default App;
