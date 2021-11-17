a8a8fb1f61577b21695997228ce3e5e2f23cf088
jdgnpy <616dona@gmail.com>
2021-11-17 13:20:46 -0300

    Add - fetch y mapeo de datos

--- src/App.js
+++ src/App.js
@@ -3,55 +3,81 @@ import './App.css';
 
 
 function App() {
-  const [pelicula, setPelicula] = useState({busqueda: '', resultado: {}});
-  const [result, setResult] = useState();
+  const [pelicula, setPelicula] = useState('');
+  const [bpeli, setBpeli] = useState(null);
+  //const [loading, setLoading] = useState(true);
+  //const [error, setError] = useState(null);
+  const [result, setResult] = useState('');
+
+//---- hook use effect ----
+  useEffect(() => {
+    fetch("http://www.omdbapi.com/?apikey=c7a38bee&t="+ pelicula +"&plot=full")
+    .then(response =>{
+      if (response.ok) {
+        return response.json();
+      }
+      throw response;
+    })
+    .then(data =>{
+      setBpeli(data);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+    //.finally(()=>{
+    //  setLoading(false);
+    //})
+  },[pelicula]);
 
 
-  const handleSubmit = (event) => {
+//---- funcion click buscar ----
+  const fClick = (event) => {
     event.preventDefault();
-    //console.log(pelicula);
-
-
-    //const resultbusqueda = fetchpeliculas(pelicula.busqueda);
-    //setResult(resultbusqueda)
-
-
-    const resultado = (
-      <>
-        <p>Mostrando resultados para {pelicula.busqueda}</p>
-      </>
-    );
-
-    setResult(resultado);
-
-
-
+    //console.log(bpeli);
+    fmapeo();
+    setPelicula('');
   }
 
-
-  const fetchPeliculas = (pelicula) => {
-
+//------- funcion para mapear resultado --------
+  const fmapeo = () =>{
+    if (pelicula !== '' && bpeli.Error !== "Movie not found!"){
+      const resultado = (
+        <>
+          <p>Mostrando resultados para {pelicula}</p>
+          
+          {/* Realizar mapeo y mostrar las peliculas que trajo la busqueda */}
+          {/* Datos del fetch a utilizar - Title, Year, Runtime, Genre, Plot, Language, Poster */}
+          <img width="200px" alt={bpeli.Title} src={bpeli.Poster} />
+          <p>Titulo: {bpeli.Title}</p>
+          <p>Año: {bpeli.Year}</p>
+          <p>Duracion: {bpeli.Runtime}</p>
+          <p>Genero: {bpeli.Genre}</p>
+          <p>Lenguaje: {bpeli.Language}</p>
+          <p>Sinopsis: {bpeli.Plot}</p>
+        
+        </>
+      );
+      setResult(resultado);
+    }else{
+      setResult(<p>No fue encontrado la pelicula {pelicula}, intente buscar nuevamente</p>)
+    }
   }
-
 
 
   return (
     <>
       <h1>Info Peliculas</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form>
         <input 
-          id="busqueda"
-          onChange={(e) => setPelicula(prevSate => ({...prevSate, [e.target.id] : e.target.value}))} //cargo en pelicula.busqueda el titulo que se busco
-          value={pelicula.busqueda}
-          placeholder="Ex: 'Pulp Fiction'"
-        />
-        <input type="submit" value="Buscar"/>
-      </form>
+            onChange={(e) => setPelicula(e.target.value)}
+            value={pelicula}
+            placeholder="Ex: 'Pulp Fiction'"
+          />
+        <button onClick={fClick}>Buscar</button>
+      </form>  
 
       {result}
-
-
     </>
   );
 }
